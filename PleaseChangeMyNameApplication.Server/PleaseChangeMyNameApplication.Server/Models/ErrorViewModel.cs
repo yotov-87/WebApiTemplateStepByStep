9755a2a6c3d38ea35f36c1c872c1c9924b99388a
dimitar.yotov <dimitar.yotov@blu11.com>
2022-11-04 19:32:32 +0200

    most of the view-related staff are deleted - wwwroot,  ErrorViewModel, and Area, Views folder

--- PleaseChangeMyNameApplication.Server/PleaseChangeMyNameApplication.Server/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace PleaseChangeMyNameApplication.Server.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
